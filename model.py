c7531f1075fd58a71491e56e7b5b24765d71ffb1
ericakufa <34973224+ericakufa@users.noreply.github.com>
2018-10-18 10:42:56 +0200

    ericakufa

---
+++ model.py
@@ -0,0 +1,164 @@
+import random
+import operator
+import matplotlib.pyplot
+import time
+
+
+num_of_agents = 10
+num_of_iterations = 100
+agents = []
+
+# Make the agents.
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])
+
+# Move the agents.
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
+
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
+
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
+
+'''
+answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
+print(answer)
+'''
+
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.xlim(0, 99)
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+matplotlib.pyplot.show()
+
+# calculating distance between 2 points
+def distance_between(agents_row_a, agents_row_b):
+	return (((agents_row_a[0] - agents_row_b[0])**2) + ((agents_row_a[1] - agents_row_b[1])**2))**0.5
+	
+start=time.clock()
+end = time.clock()
+print("time = " + str(end - start))
+
+# new model.py file using agent 
+	
+import random
+import operator
+import matplotlib.pyplot
+import agentframework
+import environment
+import eat
+
+def distance_between(agents_row_a, agents_row_b):
+    return (((agents_row_a.x - agents_row_b.x)**2) + 
+    ((agents_row_a.y - agents_row_b.y)**2))**0.5
+
+num_of_agents = 10
+num_of_iterations = 100
+agents = []
+
+# Make the agents.
+for i in range(num_of_agents):
+    agents.append(agentframework.Agent())
+
+# Move the agents.
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
+        agents[i].move()
+
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
+matplotlib.pyplot.show()
+
+for agents_row_a in agents:
+    for agents_row_b in agents:
+        distance = distance_between(agents_row_a, agents_row_b)
+		
+
+		
+# interact with environment
+environment = []
+rowlist = []
+value=[]
+environment.append(rowlist)
+rowlist.append(value)
+
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
+        agents[i].move()
+        agents[i].eat()
+
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.imshow(environment)
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
+matplotlib.pyplot.show()
+
+import random
+import operator
+import matplotlib.pyplot
+import matplotlib.animation 
+
+num_of_agents = 10
+num_of_iterations = 100
+agents = []
+
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+
+#ax.set_autoscale_on(False)
+
+# Make the agents.
+for i in range(num_of_agents):
+    agents.append([random.randint(0,100),random.randint(0,100)])
+
+carry_on = True	
+	
+def update(frame_number):
+    
+    fig.clear()   
+    global carry_on
+    
+    for i in range(num_of_agents):
+        if random.random() < 0.5:
+            agents[i][0]  = (agents[i][0] + 1) % 99 
+        else:
+            agents[i][0]  = (agents[i][0] - 1) % 99
+        
+        if random.random() < 0.5:
+            agents[i][1]  = (agents[i][1] + 1) % 99 
+        else:
+            agents[i][1]  = (agents[i][1] - 1) % 99 
+        
+    if random.random() < 0.1:
+        carry_on = False
+        print("stopping condition")
+    
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i][0],agents[i][1])
+        #print(agents[i][0],agents[i][1])
+
+		
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not actually needed as we're not assigning, but clearer
+    while (a < 10) & (carry_on) :
+        yield a			# Returns control and waits next call.
+        a = a + 1
+
+
+#animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=10)
+animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
+
+
+
+matplotlib.pyplot.show()
