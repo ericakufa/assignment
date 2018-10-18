e859b3a370608ce5b7bc24cfa14d4896a2615369
ericakufa <34973224+ericakufa@users.noreply.github.com>
2018-10-18 10:44:02 +0200

    ericakufa

---
+++ agentframework.py
@@ -0,0 +1,15 @@
+def _init_(agent):
+	def method(self):
+	    a=self(a)
+	def method(move):
+		b=move(b)
+
+
+def eat(self): # can you make it eat what is left?
+    if self.environment[self.y][self.x] > 10:
+        self.environment[self.y][self.x] -= 10
+        self.store += 10
+		
+def __init__(environment,list,move,eat):
+    self.environment = environment
+    self.store = 0
